043077151be5d036ea25cbd470e73630bd741df3
anti-spades <a_valsamos@fanshaweonline.ca>
2022-11-17 21:34:29 -0500

    more work on file system

--- components/TakePictureScreen.js
+++ components/TakePictureScreen.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
 import { View, Text, Image, StyleSheet, Button } from 'react-native';
-
+import * as FileSystem from 'expo-file-system';
 import ImageSelector from '../components/ImageSelector';
 
 const TakePictureScreen = () => {
     const [selectedImage, setSelectedImage] = useState();
+    const [availableStorage, setAvilableStorage] = useState(0);
+    const [totalStorage, setTotalStorage] = useState(0);
 
+    FileSystem.getFreeDiskStorageAsync().then(freeDiskStorage => {
+        // calculate size in GB
+        setAvilableStorage(freeDiskStorage / 1073741824);
+      });
+    
+      FileSystem.getTotalDiskCapacityAsync().then(totalDiskCapacity => {
+        // calculate size in GB
+        setTotalStorage(totalDiskCapacity / 1073741824);
+      });
+
+    const saveToFile = messageForFile => {
+        const filePath = FileSystem.documentDirectory + 'MyNewTextFile.txt';
+        FileSystem.writeAsStringAsync(filePath, messageForFile, {})
+          .then(() => {
+            console.log('File was written!');
+          })
+          .catch((error) => {
+            console.log('An error occurred: ');
+            console.log(error);
+          }); 
+      };
     const imageSelectedHandler = imagePath => {
         setSelectedImage(imagePath);
+        saveToFile(imagePath)
     }
 
     return (
